fbb757021737113f924ce4cbd22bccf7e9f6086a
razielar <razielar@gmail.com>
2018-07-30 18:53:58 +0200

    Add the script 'gtf.to.bed.R'

---
+++ gtf.to.bed.R
@@ -0,0 +1,73 @@
+#!/usr/bin/env Rscript
+
+###### GTF files: 1-based
+###### Bed files: 0-based
+
+options(stringsAsFactors = FALSE)
+
+############### Libraries: 
+
+suppressPackageStartupMessages(library(optparse))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(reshape2))
+
+##### Option list using Python's style: 
+
+option_list <- list(
+  
+  make_option(c("-i", "--input"), type = "character", default = "stdin",
+              help = "input file [default=%default]", metavar = "character"),
+  make_option("--header", default = FALSE, 
+              help = "the input file has header [default=%default]"),
+  make_option(c("-o", "--output"), type = "character", default = "Gtf.to.Bed.out.bed",
+              help="output file name. Must have a bed extension (e.g. 'What.ever.bed') [default= %default]", 
+              metavar="character")
+  
+)
+
+opt_parser <-  OptionParser(option_list=option_list)
+opt <- parse_args(opt_parser)
+
+############ Read the input data
+
+
+if (opt$input == "stdin") {
+  
+  Input <- read.delim(file("stdin"), h=opt$header)
+  
+} else {
+  
+  Input <- read.delim(opt$input, h=opt$header)
+  
+}
+
+### Debugging in RStudio: 
+# Input <- read.delim("/users/rg/ramador/D_me/RNA-seq/ncRNAs/lncRNA_Identification/Final_Agreement_lncRNAs/TransDecoder/tmp.Transcripts.lncRNA.2845.GTF.txt",
+#                     header = opt$header)
+
+Bed_file <- Input[,c(1,4,5)]
+
+
+############ Change the GTF 1-based count to BED 0-based 
+
+### Formula: 0-based: start = start-1; end = end
+
+### 1) Chr, start, end: 
+
+Bed_file$V4 <- (Bed_file[,2] -1) 
+
+### 2) ID 
+
+ID <- strsplit(Input[,9], split = ";", fixed = TRUE) %>% lapply(function(x){y <- x[2]}) %>% unlist()
+
+ID <- sub(" ", "", ID) 
+
+ID <- colsplit(ID, " ", c("Type", "ID"))
+
+Bed_file$ID <- ID$ID
+
+cat("The ID used is the Transcript_ID", "\n")
+
+
+write.table(Bed_file, file = opt$output, sep = "\t", quote = FALSE, col.names = FALSE, row.names = FALSE)
+
