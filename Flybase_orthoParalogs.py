30f137240129f6997d089f79899622226b9e5580
razielar <razielar@gmail.com>
2019-02-19 16:50:31 +0100

    Now works Flybase_orthoParalogs.py

--- Flybase_orthoParalogs.py
+++ Flybase_orthoParalogs.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
 
-# # Analyze all the Orthologos of D.me
-# ## Comparison of *D. me* with: D. sechelia, D. persimilis and D. willistoni
-# ## D. sechelia (close), D. persimilis (halfway) and D. willistoni (far away)
-# ### February 15th 2019
+# # Final analysis: Orthology relationship in *D_me*
+# ## Script to create a file including all orthology relationship for the 4 fly species 
+# ### February 19th 2019
+# Written in **Python 3.5.4** <br>
+# D.melanogaster comparing: D. sechelia (close), D. persimilis (halfway) and D. willistoni (far away)
 
-##### Libraries:
-
+#Libraries: 
 import re, sys
 
 ##### 1) Input data:
 
-file_gene_list = open(sys.argv[1] , 'r') #gene_list
+file_gene_list = open(sys.argv[1] , 'r') #Gene_list
 in2 = sys.argv[2] #Orthologs list
 out= open(sys.argv[3], 'w') #Output
 out.write('D_me\tD_sechelia\tD_persimilis\tD_willistoni\tmyID\n')
@@ -23,7 +23,7 @@ gene_list = []
 for i in file_gene_list.readlines():
     gene_list.append(i.rstrip())
 
-##### 3) Select the Orthologs and Paralogs
+##### 3) Grab the genes from the other fly species:
 
 dictOrt={}
 new=0; sec=0; per=0; wil=0
@@ -45,23 +45,23 @@ for i in open(in2, 'r').readlines():
             dictOrt[key]['wil'].append(i[5])
             wil=1
         if new == 1:
-            if sec == 1:
+            if sec == 0:
                 dictOrt[key]['sec'].append('na')
-            if per == 1:
+            if per == 0:
                 dictOrt[key]['per'].append('na')
-            if wil == 1:
+            if wil == 0:
                 dictOrt[key]['wil'].append('na')
             ort= {'sec': [], 'per': [], 'wil': []}
             new=0; sec=0; per=0; wil=0
 
-if sec == 0: #With this step you make a square matrix, avoiding gaps
+if sec == 0: 
     dictOrt[key]['sec'].append('na')
 if per == 0:
     dictOrt[key]['per'].append('na')
 if wil == 0:
     dictOrt[key]['wil'].append('na')
 
-##### 4) Label the Paralogs with the same ID
+##### 4) Print the Paralogs
 
 myID=1
 for x in gene_list:
@@ -87,4 +87,5 @@ for x in gene_list:
                 i= i+1
             myID= myID+1
 
-##### END 
+
+#####
