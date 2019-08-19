e08c7fdd09428d7af7ab0c461a01432aea1b43bc
Raziel Amador <razielar@gmail.com>
2019-08-19 20:04:53 +0200

    'merge.QC_files.sh' works in our samples

--- QC_Bam.files/merge.QC_files.sh
+++ QC_Bam.files/merge.QC_files.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
-#$ -N qc-files
-#$ -t 1
-#$ -tc 1
-#$ -q short-sl7
-#$ -o /dev/null
-#$ -e /dev/null
-Input=${SGE_TASK_ID}
-path=$(/bin/sed -n ${Input}p /users/rg/isadeghi/RNAseq_path/dir2.tsv | /bin/awk '{print $1}')
+
+path=/no_backup_isis/rg/ramador/D_me/RNA-seq/grape-nf-modENCODE/QC
 
 cd ${path}/QC/
 
 for i in `ls -d */`;
 do cd $i
 #---extracting from aliment metrics
-tail -3 ${i%/}_metrics.txt | cut -f3-8,18-21|column -t >${i%/}_metrics.tsv
+tail -3 ${i%/}_metrics.txt | cut -f3-8,18-21 |column -t >${i%/}_metrics.tsv
 
 
 ###----Extracting rows from Bamqc report---###
-sed -n 20,57p ${i%/}_bamqc.txt | grep -E 'reads|bases|rate|GC' | grep '=' | awk -F "= " '{print $2}' | sed 's/[a-z]//g' | sed 's/%//g' | sed 's/(.*)//g'| tr -d '[:blank:]'| tr '\n' '\t' | sed 's/,//g'|cut -f1-4,7-8 | column -t>${i%/}_bamqc.tsv
- 
+sed -n 20,57p ${i%/}_bamqc.txt | grep -E 'reads|bases|rate|GC' | grep '=' | awk -F "= " '{print $2}' | sed 's/[a-z]//g' | sed 's/%//g' | sed 's/(.*)//g'| tr -d '[:blank:]'| tr '\n' '\t' | sed 's/,//g'|cut -f1,2,7,8,11,12 | column -t>${i%/}_bamqc.tsv
+
 ###----Extracting columns from Picard GC summary---###
   tail -3 ${i%/}_GC_summary.txt | awk '{for(i=4;i<=6;++i)printf  $i""FS ; print "\t"}'| column -t >${i%/}_GC_summary.tsv
 
@@ -32,8 +26,9 @@ done
 cat header *qcData | column -t> QC_details
 
 ###-----Adding sample names to the files---
-ls *qcData | sed -e "1iSample" | sed 's/.qcData//g' >samples 
+ls *qcData | sed -e "1iSample" | sed 's/.qcData//g' >samples
 
 
 ###---mergin sample_id and QC data, and removing comma from file---###
 paste samples QC_details | sed 's/,//g' | column -t>QC_Data.csv
+
