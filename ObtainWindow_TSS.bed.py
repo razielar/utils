b40f619c8be42858cc74ca393875d426215081c5
Raziel Amador <razielar@gmail.com>
2019-09-16 14:35:41 +0200

    Add arguments

--- ObtainWindow_TSS.bed.py
+++ ObtainWindow_TSS.bed.py
@@ -1,42 +1,30 @@
+import sys, os
 
-# coding: utf-8
+if len(sys.argv) < 4:
+    sys.exit("usage: python ObtainWindow_TSS.bed.py bed_file.bed window output.tsv")
 
-# # Obtain the promoters of lncRNAs 
-# ## September 16th 2019
-# ## Using a bed file obtain a window of 50, 100 and 200 bp
+### --- Parameters ---
+bed_file= sys.argv[1]
+window= sys.argv[2]
+output_name= sys.argv[3]
+### --- Parameters ---
 
-# In[1]:
-
-
-import sys
-import os
-
-
-# In[2]:
-
-
-os.chdir("/nfs/users2/rg/ramador/D_me/Data/Bed_files")
-
-
-# In[20]:
-
-
-bed_file= "lncRNA_DGE_0h/lncRNA.up.0h.bed"
-window= 50
+# bed_file= "lncRNA_DGE_0h/lncRNA.up.0h.bed"
+# window= 50
+# output_name= "promoters.txt"
 
 final_bed=[]
 with open(bed_file, 'r') as bed_file:
     for i in bed_file:
         i = i.rstrip().split('\t')
         #Obtain the window
-        var_1= int(i[1])-window
-        var_2= int(i[1])+window
+        var_1= int(i[1])-int(window)
+        var_2= int(i[1])+int(window)
         i[1]= var_1
         i[2]=var_2
         final_bed.append(i)
 
-## Save the results: 
-output_name= "promoters.txt"
+
 
 
 with open(output_name, 'w') as output:
