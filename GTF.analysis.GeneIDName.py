7e764de06f6877977d1fdfdf4595987d180d367b
razielar <razielar@gmail.com>
2019-10-28 12:36:22 +0100

    add transcript option

--- GTF.analysis.GeneIDName.py
+++ GTF.analysis.GeneIDName.py
@@ -14,11 +14,16 @@ parser.add_argument("-i", "--input",
 parser.add_argument("-o", "--output", default="Gtf.analysis.tsv",
                     help="Output file name [default=%(default)s]")
 parser.add_argument("-t", "--type",
-                    help="Add type of analysis either Gene or Transcript [default=%(default)s]", default="Gene")
+                    help="Add type of analysis either 'gene' or 'transcript' [default=%(default)s]", default="gene")
 
 args=parser.parse_args()
+output_name=args.output 
 
-if args.type == "Gene":
+if args.type == "gene":
+
+    print("\n")
+    print("---- GTF analysis based on:",args.type,"----","\n")
+    
     count=0
     final=[['Gene_ID', 'Gene_Name', 'Gene_Type', 'Length', 'Strand']]
     with open(args.input, 'r') as gtf:
@@ -35,9 +40,33 @@ if args.type == "Gene":
                 gene.extend([length, strand])
                 final.append(gene)
 
-output_name=args.output 
+    with open(output_name, 'w') as output:
+        for i in final:
+            output.write("{0}\t{1}\t{2}\t{3}\t{4}\n".format(i[0],i[1],i[2],i[3],i[4]))
 
-with open(output_name, 'w') as output:
-    for i in final:
-        output.write("{0}\t{1}\t{2}\t{3}\t{4}\n".format(i[0],i[1],i[2],i[3],i[4]))
+elif args.type == "transcript":
+
+    print("\n")
+    print("---- GTF analysis based on:",args.type,"----","\n")
+    
+    count=0
+    final=[['Gene_ID', 'Gene_Name', 'Transcript_ID', 'Transcript_Name' ,'Gene_Type', 'Length', 'Strand']]
+    with open(args.input, 'r') as gtf:
+        for i in gtf:
+            i= i.rstrip().split('\t')
+            if re.search(r'transcript', i[2]):
+                count += 1
+                length=int(i[4])-(int(i[3])+1)
+                strand=i[6]
+                gene_info=i[-1].split(';')[0:5]
+                gene_name=[gene_info[i].strip().replace('"','').split(' ')[1] for i in range(len(gene_info))][:-1]
+                gene_type=[gene_info[i].strip().replace('"', '').split(' ')[2] for i in range(len(gene_info)) if gene_info[i].startswith(' gene_type')]
+                gene=gene_name+gene_type
+                gene.extend([length, strand])
+                final.append(gene)
+    
+    with open(output_name, 'w') as output:
+        for i in final:
+            output.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\n".format(i[0],i[1],i[2],i[3],i[4],i[5],i[6]))
+
 
