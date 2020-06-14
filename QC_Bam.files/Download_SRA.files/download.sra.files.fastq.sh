45a6f6f79a9a8291a2ad7b4631f690db835913e7
razielar <razielar@gmail.com>
2020-06-14 15:56:17 +0200

    add scripts to download SRA files

---
+++ QC_Bam.files/Download_SRA.files/download.sra.files.fastq.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#$ -N sra
+#$ -t 1:89
+#$ -tc 2
+#$ -q rg-el7
+#$ -l virtual_free=16G,h_rt=24:00:00
+#$ -cwd
+#$ -e cluster_out/
+#$ -o cluster_out/
+#$ -pe smp 4
+
+### 1) Read the file:
+file=${SGE_TASK_ID}
+
+file=$(grep -v Run sra.download.tsv | sed -n ${file}p | awk -F "\t" '{print $1}')
+echo $file
+
+### 2) Run the command:
+
+module load SRA-Toolkit
+
+fastq-dump --split-3 --gzip $file
+
+
+
+
