9403c3bdde744f0985bbd2e900951c9f5ca87f6c
razielar <razielar@gmail.com>
2020-06-22 16:32:54 +0200

    improve for loop format and other detail in merge.QC_files.sh

--- QC_Bam.files/merge.QC_files.sh
+++ QC_Bam.files/merge.QC_files.sh
@@ -7,27 +7,28 @@ cd ${path}/QC/
 for i in `ls -d */`;
 do cd $i
 
-###---- 1) Picard CollectAlignmentSummaryMetrics: 
-tail -3 ${i%/}_metrics.txt | cut -f3-8,18-21 | column -t >${i%/}_metrics.tsv #_metrics.txt
+   ###---- 1) Picard CollectAlignmentSummaryMetrics: 
+   tail -3 ${i%/}_metrics.txt | cut -f3-8,18-21 | column -t >${i%/}_metrics.tsv #_metrics.txt
 
 
-###---- 2) Qualimap:
-sed -n 20,57p ${i%/}_bamqc.txt | grep -E 'reads|bases|rate|GC' | grep '=' | awk -F "= " '{print $2}' | sed 's/[a-z]//g' | sed 's/%//g' | sed 's/(.*)//g' | tr -d '[:blank:]'| tr '\n' '\t' | sed 's/,//g' | cut -f1,2,7,8,11,12 | column -t>${i%/}_bamqc.tsv #_bamqc.txt
+   ###---- 2) Qualimap:
+   sed -n 20,57p ${i%/}_bamqc.txt | grep -E 'reads|bases|rate|GC' | grep '=' | awk -F "= " '{print $2}' | sed 's/[a-z]//g' | sed 's/%//g' | sed 's/(.*)//g' | tr -d '[:blank:]'| tr '\n' '\t' | sed 's/,//g' | cut -f1,2,7,8,11,12 | column -t>${i%/}_bamqc.tsv #_bamqc.txt
 
-###---- 3) Picard CollectGcBiasMetrics: 
- tail -3 ${i%/}_GC_summary.txt | awk '{for(i=4;i<=6;++i)printf  $i""FS ; print "\t"}'| column -t >${i%/}_GC_summary.tsv #_GC_summary.txt
+   ###---- 3) Picard CollectGcBiasMetrics: 
+   tail -3 ${i%/}_GC_summary.txt | awk '{for(i=4;i<=6;++i)printf  $i""FS ; print "\t"}'| column -t >${i%/}_GC_summary.tsv #_GC_summary.txt
 
-#--- Merge the output from the previous 3 commands: 
-paste *bamqc.tsv *GC_summary.tsv *metrics.tsv | column -t >${i%/}.qcData
-cp *qcData ../
-cd ../
+   #--- Merge the output from the previous 3 commands: 
+   paste *bamqc.tsv *GC_summary.tsv *metrics.tsv | column -t >${i%/}.qcData
+   cp *qcData ../
+   cd ../
+
 done
 
 ###--- Concatenate the 'header' file with all *.qcData files 
-cat header *qcData | column -t> QC_details 
+cat header *qcData | column -t > QC_details 
 
 ###----- Generate the file with the 'sample names'
-ls *qcData | sed -e "1iSample" | sed 's/.qcData//g' >samples
+ls *qcData | sed -e "1iSample" | sed 's/.qcData//g' > samples
 
 
 ###---mergin sample_id and QC data, and removing comma from file---###
