868936dfee5786d382d0d0cb00b103604ae76315
razielar <razielar@gmail.com>
2020-06-26 14:55:55 +0200

    add exon option to GTF.analysis.GeneIDName.py

--- GTF.analysis.GeneIDName.py
+++ GTF.analysis.GeneIDName.py
@@ -14,7 +14,7 @@ parser.add_argument("-i", "--input",
 parser.add_argument("-o", "--output", default="Gtf.analysis.tsv",
                     help="Output file name [default=%(default)s]")
 parser.add_argument("-t", "--type",
-                    help="Add type of analysis either 'gene' or 'transcript' [default=%(default)s]", default="gene")
+                    help="Add type of analysis either 'gene', 'transcript' or 'exon' [default=%(default)s]", default="gene")
 
 args=parser.parse_args()
 output_name=args.output 
@@ -64,9 +64,40 @@ elif args.type == "transcript":
                 gene=gene_name+gene_type
                 gene.extend([length, strand])
                 final.append(gene)
-    
+                
     with open(output_name, 'w') as output:
         for i in final:
             output.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\n".format(i[0],i[1],i[2],i[3],i[4],i[5],i[6]))
+
+
+elif args.type == "exon":
+
+    print("\n")
+    print("---- GTF analysis based on:",args.type,"----","\n")
+
+    count=0
+    final=[['Gene_ID', 'Gene_Name', 'Transcript_ID', 'Transcript_Name',
+            'Gene_Type', 'Genomic_Feature', 'Length', 'Strand']]
+    with open(args.input, 'r') as gtf:
+        for i in gtf:
+            i=i.strip().split('\t')
+            if re.search(r'exon', i[2]):
+                count += 1
+                length=int(i[4])-(int(i[3])+1)+int(1)
+                strand=i[6]
+                feature=i[2]
+                exon=i
+                gene_info=i[-1].split(';')[0:5]
+                gene_name=[gene_info[i].strip().replace('"','').split(' ')[1] for i in range(len(gene_info))][:-1]
+                gene_type=[gene_info[i].strip().replace('"', '').split(' ')[2] for i in range(len(gene_info)) if gene_info[i].startswith(' gene_type')]
+                gene=gene_name+gene_type
+                gene.extend([feature,length, strand])
+                final.append(gene)
+
+    with open(output_name, 'w') as output:
+        for i in final:
+            output.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\n".format(i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7]))
+
+
 
 
